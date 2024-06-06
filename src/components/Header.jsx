7945b678d37139946ec3e7c26efd73da506797fc
JurreWindows <jurreveijk13@gmail.com>
2024-06-06 12:24:46 +0200

    able to get different images form mock database

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Header() {
+  return (
+    <nav className="navbar">
+      <img
+        className="trollface"
+        src="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png"
+      />
+      <h1> Meme generator </h1>
+      <h2> React Course Project-3</h2>
+    </nav>
+  );
+}
+export default Header;
